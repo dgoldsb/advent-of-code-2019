884a707cab3c5237f2a6a2331cf4b6916a348b15
dgoldsb <dgoldsb@live.nl>
2019-12-01 08:22:43 +0100

    Made solution slightly nicer

--- day_1.py
+++ day_1.py
@@ -9,18 +9,22 @@ inputs = puzzle.input_data.splitlines()
 
 # PART 1
 
-puzzle.answer_a = sum([(int(x) // 3) - 2 for x in inputs])
+def find_fuel(weight: int):
+    return (weight // 3) - 2
+
+
+puzzle.answer_a = sum([find_fuel(int(x)) for x in inputs])
 
 
 # PART 2
 
-def find_fuel(weight: int):
+def find_recursive_fuel(weight: int):
     if weight < 9:
         return 0
     else:
-        fuel = (weight // 3) - 2
+        fuel = find_fuel(weight)
 
-        return fuel + find_fuel(fuel)
+        return fuel + find_recursive_fuel(fuel)
 
 
-puzzle.answer_b = sum([find_fuel(int(x)) for x in inputs])
+puzzle.answer_b = sum([find_recursive_fuel(int(x)) for x in inputs])
