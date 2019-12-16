9caf42265326c5e75bf8f48eb6a1ec60f10a9478
dgoldsb <dgoldsb@live.nl>
2019-12-16 13:14:55 +0100

    Rewrote to matrix multiplication, generating the matrix takes too long for part 2

--- day_16.py
+++ day_16.py
@@ -1,3 +1,6 @@
+import numpy as np
+from functools import lru_cache
+
 from aocd.models import Puzzle
 
 import aoc
@@ -7,43 +10,43 @@ puzzle = Puzzle(year=2019, day=16)
 inputs = puzzle.input_data
 
 
-SWITCH = {
-    0: 0,
-    1: 1,
-    2: 0,
-    3: -1,
-}
+@lru_cache()
+def generate_matrix(length: int):
+    # TODO: This is too slow.
+    switch = {
+        0: 0,
+        1: 1,
+        2: 0,
+        3: -1,
+    }
 
+    matrix = []
+    for i in range(length):
+        row = []
+        for j in range(length):
+            row.append(switch[((j + 1) % (4 * (i + 1)) // (i + 1))])
+        matrix.append(row)
 
-def generate_pattern(index: int, length: int):
-    counter = 1
-
-    for _ in range(length):
-        yield SWITCH[(counter % (4 * (index + 1)) // (index + 1))]
-        counter += 1
+    return np.array(matrix)
 
 
 def apply_pattern(subject: str) -> str:
-    result = []
+    vector_in = np.array([int(i) for i in subject])
 
-    list_subject = [int(i) for i in subject]
+    vector_out = generate_matrix(len(subject)).dot(vector_in)
 
-    for index in range(len(list_subject)):
-        digit = 0
+    string_out = ""
 
-        for x, y in zip(list_subject, generate_pattern(index, len(list_subject))):
-            digit += x * y
+    for number in vector_out:
+        string_out += str(number)[-1]
 
-        digit = str(digit)[-1]
-        result.append(digit)
-
-    return "".join(result)
+    return string_out
 
 
 def iterate_pattern(subject: str, iterations: int):
     current = subject
 
-    for _ in range(iterations):
+    for i in range(iterations):
         current = apply_pattern(current)
     return current
 
@@ -54,3 +57,15 @@ assert iterate_pattern("12345678", 4) == "01029498"
 # PART 1
 
 puzzle.answer_a = iterate_pattern(inputs, 100)[:8]
+
+
+# PART 2
+
+def calculate_part_2(subject: str):
+    output = iterate_pattern(subject * 10000, 100)
+    offset = int(output[:7])
+    return output[offset:offset + 8]
+
+
+print(calculate_part_2("03036732577212944063491565474664"))
+#assert calculate_part_2("03036732577212944063491565474664") == "84462026"
