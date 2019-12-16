8be764bbef4bea82df76558729ab49a06156b58a
dgoldsb <dgoldsb@live.nl>
2019-12-16 15:59:44 +0100

    Faster version, but too much memory usage

--- day_16.py
+++ day_16.py
@@ -1,4 +1,6 @@
+import logging
 import numpy as np
+import sys
 from functools import lru_cache
 
 from aocd.models import Puzzle
@@ -12,20 +14,20 @@ inputs = puzzle.input_data
 
 @lru_cache()
 def generate_matrix(length: int):
-    # TODO: This is too slow.
-    switch = {
-        0: 0,
-        1: 1,
-        2: 0,
-        3: -1,
-    }
-
+    # TODO: This is 10GB for the full thing, fuck.
     matrix = []
+
+    logging.info("Starting matrix generation of size %dx%d", length, length)
+
     for i in range(length):
-        row = []
-        for j in range(length):
-            row.append(switch[((j + 1) % (4 * (i + 1)) // (i + 1))])
-        matrix.append(row)
+        logging.debug("Generating row %d/%d", i, length)
+        row = [0] * i
+        pattern = [1] * (i + 1) + [0] * (i + 1) + [-1] * (i + 1) + [0] * (i + 1)
+        mult = (length - i) // len(pattern) + 1
+        row += pattern * mult
+        matrix.append(row[:length])
+
+    logging.info("Finished matrix generation of size %dx%d", length, length)
 
     return np.array(matrix)
 
@@ -33,7 +35,9 @@ def generate_matrix(length: int):
 def apply_pattern(subject: str) -> str:
     vector_in = np.array([int(i) for i in subject])
 
+    logging.info("Start multiplying matrix with vector...")
     vector_out = generate_matrix(len(subject)).dot(vector_in)
+    logging.info("Finished multiplying matrix with vector")
 
     string_out = ""
 
@@ -67,5 +71,6 @@ def calculate_part_2(subject: str):
     return output[offset:offset + 8]
 
 
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 print(calculate_part_2("03036732577212944063491565474664"))
 #assert calculate_part_2("03036732577212944063491565474664") == "84462026"
