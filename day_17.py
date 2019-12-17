e69a194255f38862800fe2b0798a20afd1c6718d
dgoldsb <dgoldsb@live.nl>
2019-12-17 17:45:12 +0100

    Day 17 part 2 done-ish, want to do the substrings automatically still

--- day_17.py
+++ day_17.py
@@ -1,4 +1,5 @@
 import asyncio
+from copy import copy
 from functools import partial
 
 from aocd.models import Puzzle
@@ -39,23 +40,72 @@ def alignment_parameter(p):
 
 
 pixels = asyncio.run(view())
-print("".join(pixels))
 
 
-r = 0
-c = 0
+row = 0
+col = 0
 walkway = set()
 for pixel in pixels:
     if pixel in ("#", ">", "<", "v", "^"):
-        walkway.add((r, c))
+        walkway.add((row, col))
 
     if pixel == "\n":
-        r += 1
-        c = 0
+        row += 1
+        col = 0
     else:
-        c += 1
+        col += 1
 
 
 puzzle.answer_a = sum(
     map(alignment_parameter, set(filter(partial(is_intersection, w=walkway), walkway)))
 )
+
+
+# PART 2
+
+async def run_roomba(m, a, b, c):
+    program = copy(inputs)
+    program[0] = 2
+    roomba = aoc.IntcodeEmulator(program=program, inputs=asyncio.Queue())
+
+    def trans(seq):
+        assert len(seq) <= 20
+        return ",".join(seq) + "\n"
+
+    commands = trans(m) + trans(a) + trans(b) + trans(c) + "n\n"
+    for command in commands:
+        roomba.inputs.put_nowait(ord(command))
+
+    await roomba.run()
+
+    outputs = []
+    while not roomba.outputs.empty():
+        outputs.append(roomba.outputs.get_nowait())
+
+    print("".join([str(chr(p)) for p in outputs[:-1]]))
+
+    return outputs[-1]
+
+
+# This was easiest solved by hand so far, with the assumption that the robot always
+# drives until it encounters a corner.
+# TODO: Solve with substrings.
+path = [
+    "L,6", "R,12", "L,4", "L,6",
+    "R,6", "L,6", "R,12",
+    "R,6", "L,6", "R,12",
+    "L,6", "L,10", "L,10", "R,6",
+    "L,6", "R,12", "L,4", "L,6",
+    "R,6", "L,6", "R,12",
+    "L,6", "L,10", "L,10", "R,6",
+    "L,6", "R,12", "L,4", "L,6",
+    "R,6", "L,6", "R,12",
+    "L,6", "L,10", "L,10", "R,6",
+]
+
+macro_a = ["L,6", "R,12", "L,4", "L,6"]
+macro_b = ["R,6", "L,6", "R,12"]
+macro_c = ["L,6", "L,10", "L,10", "R,6"]
+main = ["A", "B", "B", "C", "A", "B", "C", "A", "B", "C"]
+
+puzzle.answer_b = asyncio.run(run_roomba(main, macro_a, macro_b, macro_c))
