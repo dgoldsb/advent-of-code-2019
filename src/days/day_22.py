9881048020ad6337596cb67312d30d7e3292e06e
Dylan Goldsborough <dgoldsborough@flexport.com>
2023-11-13 00:37:08 +0100

    Maybe dead end, doing some reading/understanding

--- src/days/day_22.py
+++ src/days/day_22.py
@@ -1,3 +1,7 @@
+import math
+from functools import partial
+from typing import Generator, Callable
+
 from aocd.models import Puzzle
 
 import src.module.io  # set the session cookie
@@ -5,6 +9,7 @@ import src.module.io  # set the session cookie
 puzzle = Puzzle(year=2019, day=22)
 
 
+# PART 1
 DECK_SIZE = 10007
 
 
@@ -60,33 +65,127 @@ def parse_operations():
     return operations
 
 
-# PART 1
-
 deck = list(range(0, DECK_SIZE))
-
 for operation in parse_operations():
     deck = operation[0](deck, operation[1])
-
 puzzle.answer_a = deck.index(2019)
 
 
 # PART 2
-# We need to reverse engineer this one, first of all. We need to track card at index
-# 2020 to where it is after one shuffle cycle. We can see each shuffle cycle as a
-# constant operation, so here too we need to figure out a cycle.
+def parse_reverse_operations(inputs: list[str]) -> Generator[Callable[[int, int], int], None, None]:
+    for line in inputs[::-1]:
+        match line.split(" "):
+            case ["deal", "into", "new", "stack"]:
+                yield reverse_deal_new
+            case ["cut", arg]:
+                yield partial(reverse_cut, int(arg))
+            case ["deal", "with", "increment", arg]:
+                yield partial(reverse_deal_increment, int(arg))
 
 
-def r_deal_new(index, deck_size):
-    return (-1 - index) % deck_size
+def reverse_deal_new(index_: int, deck_size: int):
+    """Find the index before doing a deal new operation on a deck of cards of a given size.
+
+    Args:
+        index_: The index of the card we want to find the before index of.
+        deck_size: The size of the deck.
+    """
+    # This is essentially a reverse operation.
+    return (-1 - index_) % deck_size
 
 
-def r_deal(index, deck_size, increment):
-    pass
+assert reverse_deal_new(0, 10) == 9
+assert reverse_deal_new(9, 10) == 0
+assert reverse_deal_new(4, 10) == 5
 
 
-def r_cut(index, deck_size, integer):
-    return (index + integer) % deck_size
+def reverse_cut(cut_at: int, index_: int, deck_size: int):
+    """Find the index before doing a deal new operation on a deck of cards of a given size.
+
+    Args:
+        cut_at: The number of cards to cut, this section of the top is moved to the bottom of the deck.
+        index_: The index of the card we want to find the before index of.
+        deck_size: The size of the deck.
+    """
+    if cut_at < 0:
+        if index_ < -1 * cut_at:
+            return index_ + (deck_size + cut_at)
+        else:
+            return index_ + cut_at
+
+    if index_ < cut_at:
+        return index_ + cut_at
+    else:
+        return index_ - (deck_size - cut_at)
 
 
-# TODO: Apparently this is not doable without diving into modular arithmatic,
-#  saving this for later...
+assert reverse_cut(3, 8, 10) == 1
+assert reverse_cut(3, 2, 10) == 5
+assert reverse_cut(3, 9, 10) == 2
+assert reverse_cut(-4, 5, 10) == 1
+assert reverse_cut(-4, 3, 10) == 9
+assert reverse_cut(-4, 5, 10) == 1
+
+
+def reverse_deal_increment(increment_by: int, index_: int, deck_size: int) -> int:
+    """Find the index before doing a deal with increment operation on a deck of cards of a given size.
+
+    Args:
+        increment_by: The number of cards to increment by.
+        index_: The index of the card we want to find the before index of.
+        deck_size: The size of the deck.
+    """
+    # First, we figure out how many "laps" we need to do through the deck before we reach the target index.
+    # TODO: Yeah this is wrong.
+    target_lap = (increment_by - (index_ % increment_by)) % increment_by
+
+    # Second, we need to know how many cards to deal in the target lap we need to deal before we reach the target index.
+    stop_in_target_lap = index_ // increment_by
+
+    # Finally, we need to find the number of cards already dealt in the previous laps.
+    numbers_in_previous_laps = 0
+    for i in range(target_lap):
+        effective_deck_size = deck_size - i
+        lap_size = math.ceil(effective_deck_size / increment_by)
+        numbers_in_previous_laps += lap_size
+
+    # Add the numbers in the previous laps to the number of cards to deal in the target lap.
+    return numbers_in_previous_laps + stop_in_target_lap
+
+
+assert reverse_deal_increment(7, 2, 10) == 4
+
+assert reverse_deal_increment(3, 0, 10) == 0
+assert reverse_deal_increment(3, 1, 10) == 7
+assert reverse_deal_increment(3, 2, 10) == 4
+assert reverse_deal_increment(3, 3, 10) == 1
+assert reverse_deal_increment(3, 4, 10) == 8
+assert reverse_deal_increment(3, 5, 10) == 5
+assert reverse_deal_increment(3, 6, 10) == 2
+assert reverse_deal_increment(3, 7, 10) == 9
+assert reverse_deal_increment(3, 8, 10) == 6
+assert reverse_deal_increment(3, 9, 10) == 3
+
+test_input = """deal with increment 7
+deal into new stack
+deal into new stack"""
+index = 3
+deck_size = 10
+for operation in parse_reverse_operations(test_input.splitlines()):
+    result = operation(index, deck_size)
+    index = result
+print(index)
+assert index == 9
+
+
+index = int(puzzle.answer_a)
+for operation in parse_reverse_operations(puzzle.input_data.splitlines()):
+    index = operation(index, DECK_SIZE)
+print(index)
+assert index == 2019
+
+# TODO:
+#  - Fix reverse_deal_increment
+#  - Verify with reversing some test inputs
+#  - Verify with reversing the input for A
+#  - Figure out the pattern
